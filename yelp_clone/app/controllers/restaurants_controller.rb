8d1f9f5b30faf74bb147300a5fea68fb7c8863e6
souljuse <oppo.irene@gmail.com>
2016-11-14 18:15:20 +0100

    chance to edit restaurant

---
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -0,0 +1,32 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to '/restaurants'
+  end
+end
