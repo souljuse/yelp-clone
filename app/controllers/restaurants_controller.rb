d97e74e9108ac1de46cf55f6a6fd0807b27b98a8
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-17 12:12:38 +0000

    1) Modified the creating restaurant to only validate those with a name longer than 3 characters

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,23 +8,23 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    Restaurant.create(restaurant_params)
-    redirect_to '/restaurants'
-  end
-
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :description)
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render 'new'
+    end
   end
 
   def edit
-   @restaurant = Restaurant.find(params[:id])
- end
+    @restaurant = Restaurant.find(params[:id])
+  end
 
- def show
-  @restaurant = Restaurant.find(params[:id])
- end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 
- def update
+  def update
     @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
 
@@ -36,5 +36,11 @@ class RestaurantsController < ApplicationController
     @restaurant.destroy
     flash[:notice] = 'Restaurant deleted successfully'
     redirect_to '/restaurants'
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description)
   end
 end
